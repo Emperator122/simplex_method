7dd83b21ac132d2720c215f74cabb30dfc3d161d
Emperator122 <empirefp@mail.ru>
2021-12-11 02:06:41 +0300

    add table screen, add data input screen

--- main.py
+++ main.py
@@ -1,14 +1,61 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
 from libs.simplex_method.simplex_method import SimplexMethod, Sign
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+ )
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition, WipeTransition
 
-function = [1, 3]
-conditions = [
-    ([1, 1, 2], Sign.LEQ),
-    ([2, 3, 6], Sign.GEQ),
-]
-sm = SimplexMethod.problem(function, conditions)
-sm_result = sm.calculate()
-print(sm_result)
-if sm_result is not None:
-    for matrix in sm_result.simplex_tables:
-        print(matrix)
 
+class Navigator(ScreenManager):
+    input_screen = ObjectProperty(None)
+    simplex_tables_screen = ObjectProperty(None)
+
+    sm_result = ObjectProperty(None)
+
+    def open_screen(self, name, attributes: dict = None):
+        target_screen = self.get_screen(name)
+        if attributes is not None:
+            for attr_name, attr_value in attributes.items():
+                setattr(target_screen, attr_name, attr_value)
+        self.current = name
+
+
+class SimplexTableScreen(Screen):
+    sm_result = ObjectProperty(None)
+
+
+class LimitationWidget(BoxLayout):
+    coefficients = ObjectProperty(None)
+    sign = ObjectProperty(None)
+    free_coefficient = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'horizontal'
+
+
+class InputScreen(Screen):
+    f = ObjectProperty(None)
+    limitations = ObjectProperty([])
+
+    def calculate(self):
+        function = [1, 3]
+        conditions = [
+            ([1, 1, 2], Sign.LEQ),
+            ([2, 3, 6], Sign.GEQ),
+        ]
+        sm = SimplexMethod.problem(function, conditions)
+        sm_result = sm.calculate()
+        return sm_result
+
+
+class PongApp(App):
+    def build(self):
+        #SimplexTableScreen(sm_result=sm_result)
+        return Navigator(transition=WipeTransition())
+
+
+if __name__ == '__main__':
+    PongApp().run()
---
+++ widgets/simplex_table.py
@@ -0,0 +1,66 @@
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty
+ )
+
+
+Builder.load_file('widgets/simplex_table_view.kv')
+
+
+class BorderedLabel(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class SimplexTableGrid(GridLayout):
+    current_index = NumericProperty(0)
+    sm_result = ObjectProperty(None)
+    count = NumericProperty(0)
+
+    @property
+    def has_next(self) -> bool:
+        return self.current_index + 1 < len(self.sm_result.simplex_tables)
+
+    @property
+    def has_previous(self) -> bool:
+        return self.current_index - 1 >= 0
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build_table(self, table_index=0):
+        def add_column(text):
+            self.add_widget(BorderedLabel(text=text))
+        self.clear_widgets()
+        table = self.sm_result.simplex_tables[table_index]
+        self.cols = table.values.shape[1] + 1
+        # header
+        add_column('/')
+        for item in table.header:
+            add_column(str(item))
+
+        for i in range(len(table.basis)):
+            basis_item = table.basis[i]
+            add_column(str(basis_item))
+            for j in range(table.values.shape[1]):
+                add_column(str(table.values[i, j]))
+
+    def load_next(self):
+        if not self.has_next:
+            return
+        self.current_index += 1
+        self.build_table(table_index=self.current_index)
+
+    def load_previous(self):
+        if not self.has_previous:
+            return
+        self.current_index -= 1
+        self.build_table(table_index=self.current_index)
+
+    def on_sm_result(self, instance, value):
+        if self.sm_result is not None:
+            self.count = len(self.sm_result.simplex_tables)
+            self.build_table(table_index=self.current_index)
