fe3ebbf94d18a1f952148549c357d2aaaa6d7101
Emperator122 <empirefp@mail.ru>
2021-12-14 02:07:22 +0300

    add ErrorBubble

--- simplex_table_output/simplex_table_screen.py
+++ simplex_table_output/simplex_table_screen.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.uix.bubble import Bubble
+from kivy.properties import ObjectProperty, StringProperty, Property
 from kivy.lang import Builder
 
 
@@ -8,3 +9,31 @@ Builder.load_file('simplex_table_output/simplex_table_screen.kv')
 
 class SimplexTableScreen(Screen):
     sm_result = ObjectProperty(None)
+    error_bubble_box = ObjectProperty(None)
+
+    def on_sm_result(self, instance, value):
+        self.error_bubble_initialize()
+
+    def error_bubble_initialize(self, show_error=True):
+        self.error_bubble_box.clear_widgets()
+        if show_error and self.sm_result.error:
+            self.error_bubble_box.size_hint = (0.5, 1)
+            self.error_bubble_box.add_widget(
+                ErrorBubble(
+                    error_text='При вычислении произошла следующая ошибка:\r\n%s\r\nНажмите для закрытия уведомления'
+                               % self.sm_result.error_message,
+                    on_tap=self.error_bubble_initialize
+                )
+            )
+        else:
+            self.error_bubble_box.size_hint = (None, None)
+            self.error_bubble_box.height = 0
+
+
+class ErrorBubble(Bubble):
+    on_tap = None
+    error_text = StringProperty('Неизвестная ошибка')
+
+    def __init__(self, **kwargs):
+        self.on_tap = kwargs.pop('on_tap')
+        super().__init__(**kwargs)
